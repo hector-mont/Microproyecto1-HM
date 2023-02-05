dc8bdfde647b91c32bd96c0b24474a0a4ef77168
Hector Montserrat <hector.montserrat@correo.unimet.edu.ve>
2023-02-05 14:58:24 -0400

    la piedra

--- main.js
+++ main.js
@@ -36,5 +36,67 @@ if(direccion1=='adelante'){
 
 }
 
+/* Email*/
+
+const validatedEmail = (email) => {
+  return /^[a-zA-Z0-9!#$%&'*+/=?¿^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/.test(
+    email
+  );
+};
+
+const validateName = (name) => {
+  return /^[a-zA-Z\s]+(?![0-9!#$%&'*+/=?¿^_`{|}~-])$/.test(name);
+};
+
+const validateName2 = (name) => {
+  validate = true;
+  name.split("").forEach((e) => {
+    if (!isNaN(e) || e == "'" || e == "(" || e == "@") {
+      validate = false;
+    }
+  });
+  console.log(validate);
+  return validate;
+};
+
+const resetForm = (email, nombre) => {
+  email.style.borderColor = "gray";
+  nombre.style.borderColor = "gray";
+};
+
+const submitForm = (e) => {
+  e.preventDefault();
+  const email = document.getElementById("email");
+  const nombre = document.getElementById("name");
+  const mensaje = document.getElementById("mensaje");
+
+  const valueEmail = email.value;
+  const valueNombre = nombre.value;
+  const valueMensaje = mensaje.value;
+
+  resetForm(email, nombre);
+
+  if (validatedEmail(valueEmail) && validateName(valueNombre)) {
+    console.log({
+      Nombre: valueNombre,
+      email: valueEmail,
+      mensaje: valueMensaje,
+    });
+    alert("Formulario envido con exito");
+  } else if (!validateName(valueNombre) && !validatedEmail(valueEmail)) {
+    email.style.borderColor = "red";
+    nombre.style.borderColor = "red";
+    alert("correo y nombre no validos");
+  } else if (!validatedEmail(valueEmail)) {
+    alert("Correo no valido");
+    email.style.borderColor = "red";
+    console.log("formulario no enviado");
+  } else if (!validateName(valueNombre)) {
+    console.log("formulario no enviado");
+    nombre.style.borderColor = "red";
+    alert("Nombre no valido");
+  }
+};
+
 
 
